50fb2f883f269f13e9317d3f43d6e24c529ea21b
Santiago Lopez <darkomen@air.local>
2014-01-16 13:32:18 +0100

    Added a variables type ENUM for the position of the sensor,servos and the color of the line

--- software/Configuration.h
+++ software/Configuration.h
@@ -9,6 +9,7 @@
 #define MAX_MOTORS 2 //-- Not recommended to change this, as the Miniskybot code has been developed for a 2-wheel robot
 #define MAX_SENSORS_US 2
 #define MAX_SENSORS_IR 1
+#define MAX_SENSORS_FOLLOW 2
 
 //-- Table containing all the alpha and beta parameters pairs for all IR sensors
 static const float CALIBRATION_PARAM[MAX_SENSORS_IR][2] = {
@@ -16,12 +17,18 @@ static const float CALIBRATION_PARAM[MAX_SENSORS_IR][2] = {
 };
 
 //-- Types of sensors:
-#define IR 0 		//-- Infrared sensor 
+#define IR 0 		//-- Infrared sensor
 #define US 1		//-- Ultrasound sensor, generic
 #define US_3PIN 1 	//-- Ultrasound sensor, 3 pin
 #define US_4PIN 2 	//-- Ultrasound sensor, 4 pin
+#define FOLLOW 3     //-- Infrared sensor CNY70 compatible
 
 
+//Variable type ENUM for the position of the servos and sensor.
+enum pos {LEFT=0, RIGHT=1, NONE=-1};
+
+//Variable type ENUM for the color of the line.
+enum line {BLACK=0, WHITE=1};
 //-- Table containing all the pairs (velocity/value)
 //-- Must be ordered from higher to lower values
 #define NUM_VALUES 13
